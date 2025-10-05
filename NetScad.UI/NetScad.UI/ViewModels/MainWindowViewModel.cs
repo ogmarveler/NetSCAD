0c54a50846ed1562768c9cb90531b56254216842
ogmarveler <ogmarveler>
2025-10-05 02:16:49 -0500

    ILLink Task Additions

--- NetScad.UI/NetScad.UI/ViewModels/MainWindowViewModel.cs
+++ NetScad.UI/NetScad.UI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
+using Avalonia;
 using NetScad.UI.Views;
 using ReactiveUI;
+using System.Windows.Input;
 
 namespace NetScad.UI.ViewModels
 {
@@ -9,7 +11,17 @@ namespace NetScad.UI.ViewModels
         public object _mainViewContent;
 
         //public MainWindowViewModel() => _mainViewContent = new MainView();
-        public MainWindowViewModel() => _mainViewContent = new CreateAxesView(); // Start with this view
+        public MainWindowViewModel()
+        {
+            _mainViewContent = new CreateAxesView(); // Start with this view
+            // Initialize menu commands
+            NewCommand = ReactiveCommand.Create(() => { /* Handle New */ });
+            OpenCommand = ReactiveCommand.Create(() => { /* Handle Open */ });
+            CutCommand = ReactiveCommand.Create(() => { /* Handle Cut */ });
+            CopyCommand = ReactiveCommand.Create(() => { /* Handle Copy */ });
+            PasteCommand = ReactiveCommand.Create(() => { /* Handle Paste */ });
+            AboutCommand = ReactiveCommand.Create(() => { /* Show About dialog */ });
+        }
         public MainWindowViewModel(object currentView) => _mainViewContent = currentView;
 
         public object MainViewContent
@@ -19,5 +31,13 @@ namespace NetScad.UI.ViewModels
         }
 
         public void LoadCreateAxesView() => _mainViewContent = new CreateAxesView();
+
+        public ICommand NewCommand { get; }
+        public ICommand OpenCommand { get; }
+        public ICommand ExitCommand { get; }
+        public ICommand CutCommand { get; }
+        public ICommand CopyCommand { get; }
+        public ICommand PasteCommand { get; }
+        public ICommand AboutCommand { get; }
     }
 }
